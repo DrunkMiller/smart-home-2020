27381e273e84cd39037a7b53c59808c9312e910e
unknown <DrunkMiller@DESKTOP-C5CF5G2.q>
2020-10-14 02:33:09 +0300

    refactor HallDoorEvent action

--- src/main/java/ru/sbt/mipt/oop/events/HallDoorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/events/HallDoorEventHandler.java
@@ -8,32 +8,24 @@ import ru.sbt.mipt.oop.devices.SmartHome;
 import static ru.sbt.mipt.oop.events.SensorEventType.*;
 
 public class HallDoorEventHandler implements EventHandler {
-    private Room hallRoom = null;
-    private Door hallDoor = null;
     @Override
     public void handle(SmartHome smartHome, SensorEvent event) {
+        Room hallRoom = null;
+        Door hallDoor = null;
         if (event.getType() == DOOR_CLOSED) {
-            smartHome.execute(component -> {
-                if (component instanceof Room && ((Room) component).getName().equals("hall")) {
-                    hallRoom = (Room) component;
+            smartHome.execute(room -> {
+                if (room instanceof Room && ((Room) room).getName().equals("hall")) {
+                    smartHome.execute(door -> {
+                        if (door instanceof Door && ((Door) door).getId().equals(event.getObjectId())) {
+                            smartHome.execute(light -> {
+                                if (light instanceof Light) {
+                                    ((Light) light).turnOff();
+                                }
+                            });
+                        }
+                    });
                 }
             });
-            if (hallRoom != null) {
-                hallRoom.execute(component -> {
-                    if (component instanceof Door && ((Door) component).getId().equals(event.getObjectId())) {
-                        hallDoor = (Door) component;
-                    }
-                });
-            }
-            if (hallDoor != null) {
-                smartHome.execute(component -> {
-                    if (component instanceof Light) {
-                        ((Light) component).turnOff();
-                    }
-                });
-            }
-            hallRoom = null;
-            hallDoor = null;
         }
     }
 }
